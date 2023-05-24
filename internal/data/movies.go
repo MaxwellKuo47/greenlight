aa74b672edd093d4b4d88132bea4888c8de2ac06
Maxwell Kuo <maxwellkuo47@gmail.com>
2023-05-25 00:04:18 +0800

    update

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -39,6 +39,11 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
+
+	query := `INSERT INTO movies (title, year, runtime, genres)
+					 VALUES($1, $2, $3, $4)
+					 RETURNING id, created_at, version`
+
 	return nil
 }
 
